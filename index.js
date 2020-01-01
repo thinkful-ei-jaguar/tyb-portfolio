611a6642ed89eacfad750b4f5f7cf1caf3db469b
Taylor Bradshaw <taylordyoung1@gmail.com>
2019-12-31 20:23:32 -0500

    worked on hamburger menu functionality

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const greeting = "hello, this is the script you've embeded"
 console.log(greeting);
 //basic scroll function
 $(document).ready(function(){
+    let isOpen = 0;
+    let menuHeight = $('.js-menu').css('height');
+    let menuHeightInverse = "-" + menuHeight;
+
+    $("nav").on('click', '.nav-toggle', function(event) {
+        if (isOpen !== 0){
+            $('.nav-toggle').removeClass('fa-times');
+            $('.nav-toggle').addClass('fa-bars');
+            $('.js-menu').addClass('hidden');
+            isOpen++;
+        }
+        else {
+            $('.nav-toggle').removeClass('fa-bars');
+            $('.nav-toggle').addClass('fa-times');
+            $('.js-menu').removeClass('hidden');
+            isOpen--;
+        }    
+    });
+
     $(".js-scroll").on('click', function(event) {
 
       if (this.hash !== "") {
@@ -15,6 +34,7 @@ $(document).ready(function(){
         }, 500, function(){
           window.location.hash = hash;
         });
-      }
+      };
+   
     });
   });
